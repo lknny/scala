c5e190501c7986d8df9c750e89688a0d589c8ab0
Kun Liu <lknny@163.com>
2019-08-09 10:36:17 +0800

    up

---
+++ src/com/github/lknny/test/Main.scala
@@ -0,0 +1,13 @@
+package com.github.lknny.test
+
+/**
+  *
+  *
+  *
+  * Created by ${ lknny@163.com } on 2019/8/9.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    printf("hello world!")
+  }
+}
