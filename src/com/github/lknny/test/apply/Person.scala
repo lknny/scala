3e7f12f907de6a14fe27752c334a33f47d9b23f9
Kun Liu <lknny@163.com>
2019-08-14 16:03:24 +0800

    update

---
+++ src/com/github/lknny/test/apply/Person.scala
@@ -0,0 +1,31 @@
+package com.github.lknny.test.apply
+
+/**
+  *
+  *
+  *
+  * Created by ${ lknny@163.com } on 2019/8/14.
+  */
+class Person(s:String){
+  private var name=s
+  
+  def printName:Unit={
+    print(name)
+  }
+}
+
+
+
+object Person {
+  def apply(name:String):Person={
+    new Person(name)
+  }
+
+}
+
+object test{
+  def main(args: Array[String]): Unit = {
+    var person=Person("liukun")
+    person.printName
+  }
+}
